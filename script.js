7877602f8729b60020912085309dc1e57b4a6439
Rodolpho Toppan <rodolphotoppan@hotmail.com>
2022-07-05 13:24:07 -0300

    returning from bck

--- script.js
+++ script.js
@@ -16,61 +16,60 @@ let x = setInterval(function () {
   let seconds = Math.floor((distance % (1000 * 60)) / 1000)
 
   // Display the result in the element with id="demo"
-  document.getElementById('countDown').innerHTML =
+  document.getElementById('js-clock').innerHTML =
     days + ' : ' + hours + ' : ' + minutes + ' : ' + seconds
 
   // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x)
-    document.getElementById('countDown').innerHTML = 'EXPIRED'
+    document.getElementById('js-clock').innerHTML = 'EXPIRED'
   }
 }, 1000)
 
-// (function () {
+;(function () {
+  var deadline = '2022/07/03 19:00'
 
-//   var deadline = '2022/07/03 19:00';
+  function pad(num, size) {
+    var s = '0' + num
+    return s.substr(s.length - size)
+  }
 
-//   function pad(num, size) {
-//       var s = "0" + num;
-//       return s.substr(s.length - size);
-//   }
+  // fixes "Date.parse(date)" on safari
+  function parseDate(date) {
+    const parsed = Date.parse(date)
+    if (!isNaN(parsed)) return parsed
+    return Date.parse(date.replace(/-/g, '/').replace(/[a-z]+/gi, ' '))
+  }
 
-//   // fixes "Date.parse(date)" on safari
-//   function parseDate(date) {
-//       const parsed = Date.parse(date);
-//       if (!isNaN(parsed)) return parsed
-//       return Date.parse(date.replace(/-/g, '/').replace(/[a-z]+/gi, ' '));
-//   }
+  function getTimeRemaining(endtime) {
+    let total = parseDate(endtime) - Date.parse(new Date())
+    let seconds = Math.floor((total / 1000) % 60)
+    let minutes = Math.floor((total / 1000 / 60) % 60)
+    let hours = Math.floor((total / (1000 * 60 * 60)) % 24)
+    let days = Math.floor(total / (1000 * 60 * 60 * 24))
 
-//   function getTimeRemaining(endtime) {
-//       let total = parseDate(endtime) - Date.parse(new Date())
-//       let seconds = Math.floor((total / 1000) % 60)
-//       let minutes = Math.floor((total / 1000 / 60) % 60)
-//       let hours = Math.floor((total / (1000 * 60 * 60)) % 24)
-//       let days = Math.floor(total / (1000 * 60 * 60 * 24))
+    return { total, days, hours, minutes, seconds }
+  }
 
-//       return { total, days, hours, minutes, seconds };
-//   }
+  function clock(id, endtime) {
+    let days = document.getElementById(id + '-days')
+    let hours = document.getElementById(id + '-hours')
+    let minutes = document.getElementById(id + '-minutes')
+    let seconds = document.getElementById(id + '-seconds')
 
-//   function clock(id, endtime) {
-//       let days = document.getElementById(id + '-days')
-//       let hours = document.getElementById(id + '-hours')
-//       let minutes = document.getElementById(id + '-minutes')
-//       let seconds = document.getElementById(id + '-seconds')
+    var timeinterval = setInterval(function () {
+      var time = getTimeRemaining(endtime)
 
-//       var timeinterval = setInterval(function () {
-//           var time = getTimeRemaining(endtime);
+      if (time.total <= 0) {
+        clearInterval(timeinterval)
+      } else {
+        days.innerHTML = pad(time.days, 5)
+        hours.innerHTML = pad(time.hours, 5)
+        minutes.innerHTML = pad(time.minutes, 5)
+        seconds.innerHTML = pad(time.seconds, 2)
+      }
+    }, 1000)
+  }
 
-//           if (time.total <= 0) {
-//               clearInterval(timeinterval);
-//           } else {
-//               days.innerHTML = pad(time.days, 2);
-//               hours.innerHTML = pad(time.hours, 2);
-//               minutes.innerHTML = pad(time.minutes, 2);
-//               seconds.innerHTML = pad(time.seconds, 2);
-//           }
-//       }, 1000);
-//   }
-
-//   clock('js-clock', deadline);
-// })();
+  clock('js-clock', deadline)
+})()
